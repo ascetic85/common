8603410099e4959f9d2f7f21f97ac80082ea6da8
ascetic85 <yurenjimi@gmail.com>
2012-11-29 13:34:39 +0800

    [modify] gDirector [new] vertex2 and vertex3

--- oc/cocos2d/macro.h
+++ oc/cocos2d/macro.h
@@ -2,8 +2,8 @@
 //  macro.h
 //  Tetris
 //
-//  Created by Xu Jun on 11/27/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by ascetic85 on 11/27/12.
+//  Copyright (c) 2012 ascetic85. All rights reserved.
 //
 
 #ifndef jr_macro_h
@@ -12,7 +12,7 @@
 //#include "cocos2d.h"
 
 // macro for cocos2d
-#define gDirector       [[CCDirector  sharedDirector]
+#define gDirector       [CCDirector  sharedDirector]
 
 #define gEventDispatch  [[CCDirector sharedDirector] touchDispatcher]
 
@@ -31,5 +31,21 @@
 
 // macro for special function
 #define F(x)            [gFileUtils fullPathFromRelativePath:(x)]
+
+static inline ccVertex3F
+ccVertex3FMaker(GLfloat x, GLfloat y, GLfloat z)
+{
+	ccVertex3F v = {x,y,z};
+	return v;
+}
+#define vertex3(x,y,z) ccVertex3FMaker((x), (y), (z))
+
+static inline ccVertex2F
+ccVertex2FMaker(GLfloat x, GLfloat y, GLfloat z)
+{
+	ccVertex2F v = {x, y};
+	return v;
+}
+#define vertex2(x,y) ccVertex2FMaker((x), (y))
 
 #endif
